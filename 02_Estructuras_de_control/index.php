d1206b30a332388d7ea4f1632ba658c7b2978a9c
alvaro <alvaro23.ossa@gmail.com>
2024-09-25 11:02:02 +0200

    ejercicios clase dia 25-09

---
+++ 02_Estructuras_de_control/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+    <link rel= "stylesheet" type = "text/css" href = "estilos.css">
+</head>
+<body>
+    <table>
+        <caption> tabla de contenidos</caption>
+        <thead>
+           <tr>
+                <th>Archivo</th>
+                <th>Descripcion</th>    
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>
+                    <a href="numeros.php">numeros.php</a>
+                </td>
+                <td>
+                    Ejercicios de comparaciones con numeros.
+                </td>
+            <tr>
+                <td>
+                    <a href="fechas.php">fechas.php</a>
+                </td>
+                <td>
+                    Ejercicios de fechas con la funcion date().
+                </td>
+            </tr>
+                
+            </tr>
+        </tbody>
+    </table>
+</body>
+</html>
