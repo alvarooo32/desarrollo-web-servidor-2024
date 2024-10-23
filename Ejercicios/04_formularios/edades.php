b61d79c6cdfa82bf97a4fa9038797136631c07c9
alvaro <alvaro23.ossa@gmail.com>
2024-10-23 10:38:13 +0200

    23/10/24

--- Ejercicios/04_formularios/edades.php
+++ Ejercicios/04_formularios/edades.php
@@ -7,33 +7,35 @@
 </head>
 <body>
     <!--
-        CREAR UN FORMULARIO QUE RECIBA EL NOMBRE Y LA EDAD UNA PERSONA
-        SI LA EDAD ES MENOR QUE 18, SE MOSTRARA EL NOMBRE Y QUE ES MENOR DE EDAD
-        SI LA EDAD ESTA ENTRE 18 Y 65, SE MOSTRARA EL NOMBRE Y QUE ES MAYOR DE EDAD
-        SI LA EDAD ES MAS DE 65, SE MOSTRARA EL NOMBRE Y QUE SE HA JUBILADO
+        CREAR UN FORMULARIO QUE RECIBA EL NOMBRE Y LA EDAD DE UNA PERSONA
+
+        SI LA EDAD ES MENOR QUE 18, SE MOSTRARÁ EL NOMBRE Y QUE ES MENOR DE EDAD
+
+        SI LA EDAD ESTÁ ENTRE 18 Y 65, SE MOSTRARÁ EL NOMBRE Y QUE ES MAYOR DE EDAD
+
+        SI LA EDAD ES MAS DE 65, SE MOSTRARÁ EL NOMBRE Y QUE SE HA JUBILADO
     -->
+
     <form action="" method="post">
         <label for="nombre">Nombre</label>
-        <input type="text" name="nombre" id="nombre" placeholder="Introduzca el nombre"><br><br>
+        <input type="text" name="nombre" id="nombre"><br><br>
         <label for="edad">Edad</label>
-        <input type="text" name="edad" id="edad" placeholder="Introduzca la edad"><br><br>
-        <input type="submit" value="enviar">
+        <input type="text" name="edad" id="edad"><br><br>
+        <input type="submit" value="Enviar">
     </form>
+
     <?php
     if($_SERVER["REQUEST_METHOD"] == "POST") {
         $nombre = $_POST["nombre"];
         $edad = $_POST["edad"];
-        $resultado = match (true) {
+
+        $resultado = match(true) {
             $edad < 18 => "es menor de edad",
-            $edad >= 18 && $edad <= 65 => "es mayor de edad",
-            $edad < 65 => "se ha jubilado",
+            $edad >= 18 and $edad <= 65 => "es mayor de edad",
+            $edad > 65 => "se ha jubilado"
         };
         echo "<h1>$nombre $resultado</h1>";
-        
     }
-
-        
     ?>
-    
 </body>
 </html>
---
+++ Ejercicios/05_Funciones/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+    <link rel="stylesheet" type="text/css" href="./estilos.css">
+</head>
+<body>
+    <table>
+        <caption>Tabla de contenidos</caption>
+        <thead>
+            <tr>
+                <th>Archivo</th>
+                <th>Descripción</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>
+                    <a href="temperaturas.php">temperaturas.php</a>
+                </td>
+                <td>
+                    Funcion de temperaturas.
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <a href="edades.php">edades.php</a>
+                </td>
+                <td>
+                    Funcion de edades.
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <a href="potencias.php">potencias.php</a>
+                </td>
+                <td>
+                    Funcion potencias.
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+</html>
