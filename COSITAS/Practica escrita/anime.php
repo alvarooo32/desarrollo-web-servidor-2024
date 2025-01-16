4bb328b54934ff4d872dd9b0f17a41b68b206ed2
alvarooo32 <alvaro23.ossa@gmail.com>
2025-01-16 22:37:51 +0100

    IMPORTANTE PARA EXAMEN ESCRITO

--- COSITAS/Practica escrita/anime.php
+++ COSITAS/Practica escrita/anime.php
@@ -1,27 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Index de Animes</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <?php
-        error_reporting(E_ALL);
-        ini_set("display_errors", 1);
-
-        require('conexion.php');
-
-        // Para traernos la sesión iniciada
-        session_start();
-        if (isset($_SESSION["usuario"])) {
-            echo "<h1>Bienvenid@" . $_SESSION["usuario"] . "</h1>";
-        } else {
-            // Solo utilizar si no hay nada en el body
-            header("location: usuario/iniciar_sesion.php");
-            exit;
-        }
-    ?>
-</head>
 <body>
     <div class="container">
         <a class="btn btn-warning" href="usuario/cerrar_sesion.php">Cerrar sesión</a>
@@ -97,6 +73,5 @@
             </tbody>
         </table>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
